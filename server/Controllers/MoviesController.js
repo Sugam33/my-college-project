3a9639074c555ae339f527290db11df90dc0e339
Sugam Shakya <shakyasugam500@gmail.com>
2023-07-03 17:53:54 +0545

    added movie filter

--- server/Controllers/MoviesController.js
+++ server/Controllers/MoviesController.js
@@ -10,4 +10,60 @@ const importMovies = asyncHandler(async(req, res) => {
     res.status(201).json(movies);
 });
 
-export { importMovies };
+// get all movies,   route- GET /api/movies
+const getMovies = asyncHandler(async(req, res) => {
+    try{
+        // filter movies by category, time, language, rate, year and search
+        const{ category, time, language, rate, year, search } = req.query;
+        let query = {
+            ...(category && { category }),
+            ...(time && { time }),
+            ...(language && { language }),
+            ...(rate && { rate }),
+            ...(year && { year }),
+            ...(search && { name: { $regex: search, $options: "i" } }), 
+        }
+
+        // load more movies functionality
+        const page = Number(req.query.pageNumber) || 1; // if page number not provided in query we set it to 1
+        const limit = 2; // 2 movies per page
+        const skip = (page - 1) * limit; // skip 2 movies per page
+
+        // find movies by query, skip and limit
+        const movies = await Movie.find(query)
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit);
+
+        // get total number of movies
+        const count = await Movie.countDocuments(query);
+
+        // send response with movies and total number of movies
+        res.json({ movies, page, pages: Math.ceil(count / limit), totalMovies: count, });
+    }
+    catch(error){
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// get movie by id,   route - GET /api/movies/:id
+const getMovieById = asyncHandler(async(req, res) => {
+    try{
+        // find movie by id in database
+        const movie = await Movie.findById(req.params.id);
+        // movie vetayo vane client lai pathauney
+        if(movie){
+            res.json(movie);
+        }
+        // vetayena vane error pathau
+        else{
+            res.status(404);
+            throw new Error("Movie not found");
+        }
+    }
+    catch(error){
+        res.status(400).json({ message: error.message });
+    }
+});
+
+export { importMovies, getMovies, getMovieById };
--- server/Routes/MoviesRouter.js
+++ server/Routes/MoviesRouter.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import { admin, protect } from '../middlewares/Auth.js';
-import { importMovies } from '../Controllers/MoviesController.js';
+import { getMovies, importMovies } from '../Controllers/MoviesController.js';
 
 const router = express.Router();
 
 // ******** PUBLIC ROUTES *******
 router.post("/import", importMovies);
+router.get("/", getMovies);
+
 
 
 // ******* PROTECTED ROUTES ******
