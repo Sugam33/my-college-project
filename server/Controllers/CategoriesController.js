ec19abf54433a35b250d80e294ac76f39d96d483
Sugam Shakya <shakyasugam500@gmail.com>
2023-07-05 16:40:25 +0545

    completed categories controller

--- server/Controllers/CategoriesController.js
+++ server/Controllers/CategoriesController.js
@@ -36,3 +36,49 @@ const createCategory = asyncHandler(async(req, res) => {
         res.status(400).json({ message: error.message });
     }
 });
+
+// update category,  route - PUT /api/categories/:id
+const updateCategory = asyncHandler(async(req, res) => {
+    try{
+        // get category id from request params
+        const category = await Categories.findById(req.params.id);
+
+        if(category) {
+            // update category title
+            category.title = req.body.title || category.title;
+            // save updated category in database
+            const updatedCategory = await category.save();
+            // send updated category to client
+            res.json(updatedCategory);
+        }
+        else{
+            res.status(404).json({ message: "Category not found" });
+        }
+    }
+    catch(error){
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// delete category,   route - DELETE /api/categories/:id
+const deleteCategory = asyncHandler(async(req, res) => {
+    try{
+        // get category id from request params
+        const category = await Categories.findById(req.params.id);
+
+        if(category) {
+            // delete category from database
+            await category.deleteOne();
+            // send success message to client
+            res.json({ message: "Category removed" });
+        }
+        else{
+            res.status(404).json({ message: "Category not found" });
+        }
+    }
+    catch(error){
+        res.status(400).json({ message: error.message })
+    }
+});
+
+export { getCategories, createCategory, updateCategory, deleteCategory };
