2ce66a9480316bd7744bb6d29c51ed08748d4d85
Sugam Shakya <shakyasugam500@gmail.com>
2023-07-10 09:14:39 +0545

    created API call for add and delete movies in watchlist

--- src/Redux/APIs/userServices.js
+++ src/Redux/APIs/userServices.js
@@ -60,6 +60,26 @@ const changePasswordService = async (passwords, token) => {
   return data;
 };
 
+// get all favorite movies
+const getFavoriteMovies = async (token) => {
+  const { data } = await Axios.get("/users/favorites", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
+
+// delete all favorite movies
+const deleteFavoriteMovies = async (token) => {
+  const { data } = await Axios.delete("/users/favorites", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
+
 export {
   registerService,
   logoutService,
@@ -67,4 +87,6 @@ export {
   updateProfileService,
   deleteProfileService,
   changePasswordService,
+  getFavoriteMovies,
+  deleteFavoriteMovies,
 };
