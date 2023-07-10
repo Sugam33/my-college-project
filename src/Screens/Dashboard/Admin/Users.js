88b6979aafd112bb374ddf83c0ed0163f9eda977
Sugam Shakya <shakyasugam500@gmail.com>
2023-07-10 13:44:40 +0545

    adjusted and completed get users and delete users

--- src/Screens/Dashboard/Admin/Users.js
+++ src/Screens/Dashboard/Admin/Users.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Table2 from "../../../Components/Table2";
 import SideBar from "../SideBar";
-import { UsersData } from "../../../Data/MovieData";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  deleteUserAction,
+  getAllUsersAction,
+} from "../../../Redux/Actions/userActions";
+import { toast } from "react-hot-toast";
+import Loader from "../../../Components/Notifications/Loader";
+import { Empty } from "../../../Components/Notifications/Empty";
 
 function Users() {
+  const dispatch = useDispatch();
+
+  const { isLoading, isError, users } = useSelector(
+    (state) => state.adminGetAllUsers
+  );
+
+  // delete
+  const { isError: deleteError, isSuccess } = useSelector(
+    (state) => state.adminDeleteUser
+  );
+
+  // delete user handler
+  const deleteMoviesHandler = (id) => {
+    if (window.confirm("Are you sure you want to delete this user?")) {
+      dispatch(deleteUserAction(id));
+    }
+  };
+
+  // useEffect
+  useEffect(() => {
+    dispatch(getAllUsersAction());
+    if (isError || deleteError) {
+      toast.error(isError || deleteError);
+      dispatch({
+        type: isError ? "GET_ALL_USERS_RESET" : "DELETE_USER_RESET",
+      });
+    }
+  }, [dispatch, isError, deleteError, isSuccess]);
   return (
     <SideBar>
       <div className="flex flex-col gap-6">
         <h2 className="text-xl font-bold">Users</h2>
-
-        <Table2 data={UsersData} users={true} />
+        {isLoading ? (
+          <Loader />
+        ) : users?.length > 0 ? (
+          <Table2
+            data={users}
+            users={true}
+            onDeleteFunction={deleteMoviesHandler}
+          />
+        ) : (
+          <Empty message="No users found" />
+        )}
       </div>
     </SideBar>
   );
