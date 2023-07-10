a3e4254e9e00975402c06876ae7066bdbca8ddb0
Sugam Shakya <shakyasugam500@gmail.com>
2023-07-10 17:19:42 +0545

    created actions for categories

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,40 +1,37 @@
-import {
-    combineReducers,
-    configureStore,
-} from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import * as User from "./Reducers/userReducers";
 import * as categories from "./Reducers/CategoriesReducer";
 
 const rootReducer = combineReducers({
-    // user reducers
-    userLogin: User.userLoginReducer,
-    userRegister: User.userRegisterReducer,
-    userUpdateProfile: User.userUpdateProfileReducer,
-    userDeleteProfile: User.userDeleteProfileReducer,
-    userchangepassword: User.userChangePasswordReducer,
-    userGetFavoriteMovies: User.userGetFavoriteMoviesReducer,
-    userDeleteFavoriteMovies: User.userDeleteFavoriteMoviesReducer,
-    adminGetAllUsers: User.adminGetAllUsersReducer,
-    adminDeleteUser: User.adminDeleteUserReducer,
+  // user reducers
+  userLogin: User.userLoginReducer,
+  userRegister: User.userRegisterReducer,
+  userUpdateProfile: User.userUpdateProfileReducer,
+  userDeleteProfile: User.userDeleteProfileReducer,
+  userchangepassword: User.userChangePasswordReducer,
+  userGetFavoriteMovies: User.userGetFavoriteMoviesReducer,
+  userDeleteFavoriteMovies: User.userDeleteFavoriteMoviesReducer,
+  adminGetAllUsers: User.adminGetAllUsersReducer,
+  adminDeleteUser: User.adminDeleteUserReducer,
 
-    // Category reducers
-    categoryGetAll: categories.getAllCategoriesReducer,
-    categoryCreate: categories.createCategoryReducer,
-    categoryUpdate: categories.updateCategoryReducer,
-    categoryDelete: categories.deleteCategoryReducer,
+  // Category reducers
+  categoryGetAll: categories.getAllCategoriesReducer,
+  categoryCreate: categories.createCategoryReducer,
+  categoryUpdate: categories.updateCategoryReducer,
+  categoryDelete: categories.deleteCategoryReducer,
 });
 
 // get userInfo from localStorage
 const userInfoFromStorage = localStorage.getItem("userInfo")
-    ? JSON.parse(localStorage.getItem("userInfo"))
-    : null;
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 // initialState
 const initialState = {
-    userLogin: { userInfo: userInfoFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 export const store = configureStore({
-    reducer: rootReducer,
-    preloadedState: initialState,
+  reducer: rootReducer,
+  preloadedState: initialState,
 });
