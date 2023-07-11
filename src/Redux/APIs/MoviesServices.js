b2197442114a7469afe5498ba82558ca02988f93
Sugam Shakya <shakyasugam500@gmail.com>
2023-07-11 12:59:51 +0545

    created movies services get all movies

---
+++ src/Redux/APIs/MoviesServices.js
@@ -0,0 +1,17 @@
+import Axios from "./Axios";
+
+// get all movies function
+export const getAllMoviesService = async ({
+  category,
+  time,
+  language,
+  rate,
+  year,
+  search,
+  pageNumber,
+}) => {
+  const { data } = await Axios.get(
+    `/movies?category=${category}&time=${time}&language=${language}&rate=${rate}&year=${year}&search=${search}&pageNumber=${pageNumber}`
+  );
+  return data;
+};
