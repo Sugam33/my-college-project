05beb0a6c55c081e0d3948c3588a1bf05ed0be1c
Sugam Shakya <shakyasugam500@gmail.com>
2023-07-12 22:13:03 +0545

    created movie constants and services for displaying top rated and random movies

--- src/Redux/APIs/MoviesServices.js
+++ src/Redux/APIs/MoviesServices.js
@@ -8,10 +8,28 @@ export const getAllMoviesService = async (
   rate,
   year,
   search,
-  pageNumber,
+  pageNumber
 ) => {
   const { data } = await Axios.get(
     `/movies?category=${category}&time=${time}&language=${language}&rate=${rate}&year=${year}&search=${search}&pageNumber=${pageNumber}`
   );
+  return data;
+};
+
+// get random movies function
+export const getRandomMoviesService = async () => {
+  const { data } = await Axios.get(`/movies/random/all`);
+  return data;
+};
+
+// get movie by id function
+export const getMovieByIdService = async (id) => {
+  const { data } = await Axios.get(`/movies/${id}`);
+  return data;
+};
+
+// get top rated movie function
+export const getTopRatedMovieService = async () => {
+  const { data } = await Axios.get(`/movies/rated/top`);
   return data;
 };
