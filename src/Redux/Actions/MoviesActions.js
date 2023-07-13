a1dae04dabbd67cff5c6caed37f3bb0c665605fc
Sugam Shakya <shakyasugam500@gmail.com>
2023-07-13 12:36:34 +0545

    created required movie actions and added on homescreen

--- src/Redux/Actions/MoviesActions.js
+++ src/Redux/Actions/MoviesActions.js
@@ -34,3 +34,45 @@ export const getAllMoviesAction =
       ErrorsAction(error, dispatch, moviesConstants.MOVIES_LIST_FAIL);
     }
   };
+
+// get random movies action
+export const getRandomMoviesAction = () => async (dispatch) => {
+  try {
+    dispatch({ type: moviesConstants.MOVIES_RANDOM_REQUEST });
+    const response = await moviesAPIs.getRandomMoviesService();
+    dispatch({
+      type: moviesConstants.MOVIES_RANDOM_SUCCESS,
+      payload: response,
+    });
+  } catch (error) {
+    ErrorsAction(error, dispatch, moviesConstants.MOVIES_RANDOM_FAIL);
+  }
+};
+
+// get movie by id action
+export const getMovieByIdAction = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: moviesConstants.MOVIE_DETAILS_REQUEST });
+    const response = await moviesAPIs.getMovieByIdService(id);
+    dispatch({
+      type: moviesConstants.MOVIE_DETAILS_SUCCESS,
+      payload: response,
+    });
+  } catch (error) {
+    ErrorsAction(error, dispatch, moviesConstants.MOVIE_DETAILS_FAIL);
+  }
+};
+
+// get top rated movie action
+export const getTopRatedMovieAction = () => async (dispatch) => {
+  try {
+    dispatch({ type: moviesConstants.MOVIE_TOP_RATED_REQUEST });
+    const response = await moviesAPIs.getTopRatedMovieService();
+    dispatch({
+      type: moviesConstants.MOVIE_TOP_RATED_SUCCESS,
+      payload: response,
+    });
+  } catch (error) {
+    ErrorsAction(error, dispatch, moviesConstants.MOVIE_TOP_RATED_FAIL);
+  }
+};
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import Banner from '../Components/Home/Banner';
-import PopularMovies from '../Components/Home/PopularMovies';
+import React from "react";
+import Banner from "../Components/Home/Banner";
+import PopularMovies from "../Components/Home/PopularMovies";
 //import Promos from '../Components/Home/Promos';
-import TopRated from '../Components/Home/TopRated';
-import Layout from '../Layout/Layout';
+import TopRated from "../Components/Home/TopRated";
+import Layout from "../Layout/Layout";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import {
+  getAllMoviesAction,
+  getRandomMoviesAction,
+  getTopRatedMovieAction,
+} from "../Redux/Actions/MoviesActions";
+import { toast } from "react-hot-toast";
 
 function HomeScreen() {
+  const dispatch = useDispatch();
+  const {
+    isLoading: randomLoading,
+    isError: randomError,
+    movies: randomMovies,
+  } = useSelector((state) => state.getRandomMovies);
+  const {
+    isLoading: topLoading,
+    isError: topError,
+    movies: topMovies,
+  } = useSelector((state) => state.getTopRatedMovie);
+  const { isLoading, isError, movies } = useSelector(
+    (state) => state.getAllMovies
+  );
+
+  useEffect(() => {
+    // get random movies
+    dispatch(getRandomMoviesAction());
+    // get all movies
+    dispatch(getAllMoviesAction({}));
+    // get top rated movies
+    dispatch(getTopRatedMovieAction());
+    // errors
+    if (isError || randomError || topError) {
+      toast.error("Something is wrong");
+    }
+  }, [dispatch, isError, randomError, topError]);
+
   return (
     <Layout>
       <div className="container mx-auto min-h-screen px-2 mb-6">
         <Banner />
         <PopularMovies />
-      {/* <Promos /> */}
+        {/* <Promos /> */}
         <TopRated />
       </div>
     </Layout>
