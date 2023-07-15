706e0a5a56ed2d9a6dece12c8e81d785d076238c
Sugam Shakya <shakyasugam500@gmail.com>
2023-07-15 09:26:23 +0545

    created liked movie reducer

--- src/Redux/Reducers/userReducers.js
+++ src/Redux/Reducers/userReducers.js
@@ -157,3 +157,19 @@ export const adminDeleteUserReducer = (state = {}, action) => {
         return state;
   }
 }
+
+// USER LIKED MOVIE
+export const userLikedMovieReducer = (state = {}, action) => {
+  switch (action.type) {
+    case userConstants.LIKED_MOVIE_REQUEST:
+      return { isLoading: true };
+    case userConstants.LIKED_MOVIE_SUCCESS:
+      return { isLoading: false, isSuccess: true };
+    case userConstants.LIKED_MOVIE_FAIL:
+      return { isLoading: false, isError: action.payload };
+    case userConstants.LIKED_MOVIE_RESET:
+      return {};
+    default: 
+      return state;
+  }
+}
